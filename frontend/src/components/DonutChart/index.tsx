65a4dd0002d13bc213ddfc7366096a6162d18db2
julianapedroso <juliana.cristina@hotmail.com.br>
2021-05-07 15:27:52 -0300

    integração do gráfico de rosca

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,29 +1,38 @@
 import Chart from "react-apexcharts";
 import axios from "axios";
-import { BASE_URL } from "utils/requests"
+import { useState, useEffect } from "react";
+import { BASE_URL } from "utils/requests";
 import { SaleSum } from "types/sale";
 
 type ChartData = {
   labels: string[];
-  series: number[]
-}
+  series: number[];
+};
 
 const DonutChart = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
+    series: [],
+  });
 
-  let chartData: ChartData = { labels: [], series: []};
+  useEffect(() => {
+    showDonutChart();
+  }, []);
 
-  axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then((response) => {
-      const data = response.data as SaleSum[]
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+  const showDonutChart = () => {
+    axios
+      .get(`${BASE_URL}/sales/amount-by-seller`)
+      .then((response) => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map((x) => x.sellerName);
+        const mySeries = data.map((x) => x.sum);
 
-      chartData = { labels: myLabels, series: mySeries }
-      console.log(chartData)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+        setChartData({ labels: myLabels, series: mySeries });
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
 
   // const mockData = {
   //   series: [477138, 499928, 444867, 220426, 473088],
